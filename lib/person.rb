64e1baa2f701bd812cea3f953f72298d98e6ea3c
Michael Michael <michael.ta.chan@gmail.com>
2019-04-19 04:17:32 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def name
+    @this_person_name
+  end
+  
+  def name=(input)
+    @this_dogs_name = input
+  end
+  
+  def breed
+    @this_dogs_breed
+  end
+  
+  def breed=(input)
+    @this_dogs_breed = input
+  end
+end
